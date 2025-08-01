4751f107ae4490c709b05cf899ad919d34d6ac17
ichwansyaifani <ichwansyaifani@gmail.com>
2025-08-01 13:50:25 +0800

    demo 2

---
+++ demo2.dart
@@ -0,0 +1,12 @@
+void main() {
+  var total = 25000;
+  var productName = "GG Filter 20";
+  double discount = 0;
+
+  if (total >= 25000 && productName == "GG Filter 20") {
+    discount = 0.1;
+    print("anda mendapatkan potongan 10 %");
+  } else {
+    print("Tidak ada potongan discount");
+  }
+}
