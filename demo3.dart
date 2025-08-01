66d52acb00570b3090df353ab126ddbe24d21ec1
ichwansyaifani <ichwansyaifani@gmail.com>
2025-08-01 14:22:16 +0800

    demo 3

---
+++ demo3.dart
@@ -0,0 +1,29 @@
+//List
+void main() {
+  List ageList = [41, 42, 43, 44];
+  List randomList = ["Ichwan", 42, 43, 44];
+  List<String> listString = ["SK Filter", "GG Kretek"];
+  print(ageList);
+  print(randomList);
+  print(listString);
+
+  /*
+  1. cara mendeklarasikannya
+  2. cara mengambil nilainya
+  3. cara mengubah nilainya
+
+  */
+
+  //MAP (key,value)biasanya {}
+  // "key" : "value"
+
+  //List (Index);[]
+
+  //print(products.length);
+  // for (var i = 0; i < products.length; i++) {
+  // var product = products[i];
+
+  //print(product);
+  //print(product["product_name"]);
+  // }
+}
